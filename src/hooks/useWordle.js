87b02a1d8805daa393acd0a4516ffd2d3338044b
Shivakarka <shivakarka@gmail.com>
2022-07-06 15:03:02 +0530

    implemented logic for keypress-Enter

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,12 +4,14 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
+  const [history, setHistory] = useState(["hello"]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
   // func to format guess into array of letter objects
   // eg: [{key:'a', color:'green'}]
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log('formatting the guess - ', currentGuess)
+  };
 
   // func to add a new guess to the guess state
   // update the incorrect state if guess is wrong
@@ -20,6 +22,25 @@ const useWordle = (solution) => {
   // if user press enter , add the new guess
   const handleKeyup = ({ key }) => {
     // console.log("key pressed - ", key);
+
+    if (key === 'Enter') {
+        // only add guess if turn is less than 5
+        if (turn > 5) {
+          console.log('you used all your guesses!')
+          return
+        }
+        // do not allow duplicate words
+        if (history.includes(currentGuess)) {
+          console.log('you already tried that word.')
+          return
+        }
+        // check word is 5 characters
+        if (currentGuess.length !== 5) {
+          console.log('word must be 5 chars.')
+          return
+        }
+        formatGuess()
+      }
 
     if (key === "Backspace") {
       setCurrentGuess((prev) => prev.slice(0, -1));
