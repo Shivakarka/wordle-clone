fd1a899c5d3b3ca38ae7b4a34b0312f748621185
Shivakarka <shivakarka@gmail.com>
2022-08-22 01:32:12 +0530

    updated grid rows to show letters and past guesses

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,8 +4,11 @@ import Row from './Row'
 export default function Grid({ guesses, currentGuess, turn }) {
   return (
     <div>
+    
+    {/* guess object - g , index - i */}
+    
       {guesses.map((g, i) => {
-        return <Row key={i} /> 
+        return <Row key={i}  guess={g}/> 
       })}
     </div>
   )
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-export default function Row() {
+export default function Row({ guess }) {
+
+if (guess) {
+    return (
+      <div className="row past">
+
+        {/* letter - l , index - i */}
+
+        {guess.map((l, i) => (
+          <div key={i} className={l.color}>{l.key}</div>
+        ))}
+      </div>
+    )
+}
 
   return (
     <div className="row">
