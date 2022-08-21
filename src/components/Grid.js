3d75c3a849ed45aade542a27fe1918cfa29bee04
Shivakarka <shivakarka@gmail.com>
2022-08-22 01:48:07 +0530

    updated grid rows to show current guess letters

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Row from './Row'
+import React from "react";
+import Row from "./Row";
 
 export default function Grid({ guesses, currentGuess, turn }) {
   return (
     <div>
-    
-    {/* guess object - g , index - i */}
-    
+      {/* guess object - g , index - i */}
+
       {guesses.map((g, i) => {
-        return <Row key={i}  guess={g}/> 
+        // the below row is displayed for that particular turn ,
+        // other previous guesses will be the default row template
+
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />;
+        }
+
+        //default row
+        return <Row key={i} guess={g} />;
       })}
     </div>
-  )
+  );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React from "react";
 
-export default function Row({ guess }) {
-
-if (guess) {
+export default function Row({ guess, currentGuess }) {
+  if (guess) {
     return (
       <div className="row past">
-
         {/* letter - l , index - i */}
 
         {guess.map((l, i) => (
-          <div key={i} className={l.color}>{l.key}</div>
+          <div key={i} className={l.color}>
+            {l.key}
+          </div>
         ))}
       </div>
-    )
-}
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split("");
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">
+            {letter}
+          </div>
+        ))}
+
+        {/* if this below code is absent, new boxes will generated for each letter guess 
+        as letters will be empty initially and will render blank*/}
+
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <div className="row">
@@ -23,6 +44,5 @@ if (guess) {
       <div></div>
       <div></div>
     </div>
-  )
-  
+  );
 }
